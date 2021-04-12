f817576cf4730ecda9dc8689d4d32d398f3d8665
imdbvideo <82320857+imdbvideo@users.noreply.github.com>
2021-04-12 10:30:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,12 +49,13 @@ def reply(update, context):
     ans+='*Rated*: '+data['Rated']+' \n'
     ans+='*Runtime*: '+data['Runtime']+' \n'
     ans+='*Cast*: '+data['Actors']+'\n'
-    ans+='*Poster*: []('+data['Poster']+')\n\n'
+    ans+='*Poster*: [#Images]('+data['Poster']+')\n'
     ans+='*Genre*: '+data['Genre']+'\n\n'
     
 
     ans+='*Plot*: '+data['Plot']+'\n\n'
-    ans+='[Watch Full Movie Online](http://www.omdbapi.com/?i='+data['imdbID']+')\n'
+    ans+='[#Watch Movie Online](http://www.omdbapi.com/?i='+data['imdbID']+')\n\n'
+    ans+='[#Download Movie](http://www.omdbapi.com/?i='+data['imdbID']+')\n'
     ans+='[.]('+data['Poster']+')'
     update.message.reply_text(ans,parse_mode='markdown')  
 
