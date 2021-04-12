5b8520ec1fd542bd09ece9d7c2cded26fb8a2357
imdbvideo <82320857+imdbvideo@users.noreply.github.com>
2021-04-12 11:44:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,6 +69,7 @@ def main():
     
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("dmca", dmca))
     dp.add_handler(MessageHandler(Filters.text, reply))
     dp.add_error_handler(error)
 
